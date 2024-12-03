665f3fa001338373360f9bd16d59858f4b9be2f5
Victor Adolfsson <victoreadolfsson@gmail.com>
2024-12-03 15:22:31 +0100

    add endpoints and fetch in frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,11 +15,64 @@ client.connect();
 
 app.use(express.json());
 
-app.get('/api', async (_request: Request, response: Response) => {
+app.get('/api/todos', async (_request: Request, response: Response) => {
+  const { rows } = await client.query('SELECT * FROM todos;');
   try {
-    response.status(200).send('Hello from API');
+    response.status(200).send(rows);
   } catch (error) {
     console.error('Error executing query', error);
+  }
+});
+
+app.post('/api/todos', async (request: Request, response: Response) => {
+  const { task, completed } = request.body;
+  console.log('post', task, completed);
+  try {
+    const { rows } = await client.query(
+      'INSERT INTO todos (task, completed) VALUES ($1, $2) RETURNING *;',
+      [task, completed]
+    );
+    response.status(201).send(rows[0]);
+  } catch (error) {
+    console.error('Error executing query', error);
+    response.status(500).send('Internal Server Error');
+  }
+});
+
+app.put('/api/todos/:id', async (request: Request, response: Response) => {
+  const { id } = request.params;
+  const { task, completed } = request.body;
+  try {
+    const { rows } = await client.query(
+      'UPDATE todos SET task = $1, completed = $2 WHERE id = $3 RETURNING *;',
+      [task, completed, id]
+    );
+    if (rows.length === 0) {
+      response.status(404).send('Todo not found');
+    } else {
+      response.status(200).send(rows[0]);
+    }
+  } catch (error) {
+    console.error('Error executing query', error);
+    response.status(500).send('Internal Server Error');
+  }
+});
+
+app.delete('/api/todos/:id', async (request: Request, response: Response) => {
+  const { id } = request.params;
+  try {
+    const { rowCount } = await client.query(
+      'DELETE FROM todos WHERE id = $1;',
+      [id]
+    );
+    if (rowCount === 0) {
+      response.status(404).send('Todo not found');
+    } else {
+      response.status(204).send();
+    }
+  } catch (error) {
+    console.error('Error executing query', error);
+    response.status(500).send('Internal Server Error');
   }
 });
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import './App.css';
 
 const App: FC = () => {
+  const getTodos = async () => {
+    try {
+      const response = await fetch('api/todos');
+      const data = await response.json();
+      console.log('data', data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
   return (
     <div>
       <h1>App</h1>
