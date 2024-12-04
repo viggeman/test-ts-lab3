d2d2f47748d0083e95f33a19cf7bfe037d175a22
Victor Adolfsson <victoreadolfsson@gmail.com>
2024-12-04 14:59:02 +0100

    initial fail test tdd

--- frontend/cypress/component/TodoList.cy.tsx
+++ frontend/cypress/component/TodoList.cy.tsx
@@ -5,30 +5,12 @@ describe('TodoList Component', () => {
   it('renders "No todos yet!" when the list is empty', () => {
     mount(
       <TodoList
-        todo={[]}
+        todos={[]}
         onToggleComplete={() => {}}
         onEdit={() => {}}
         onDelete={() => {}}
       />
     );
     cy.contains('No todos yet!').should('be.visible');
-  });
-
-  it('renders a list of todos', () => {
-    const mockTodos: { id: number; task: string; completed: boolean }[] = [
-      { id: 1, task: 'Task 1', completed: false },
-      { id: 2, task: 'Task 2', completed: true },
-    ];
-    mount(
-      <TodoList
-        todo={mockTodos}
-        onToggleComplete={() => {}}
-        onEdit={() => {}}
-        onDelete={() => {}}
-      />
-    );
-    cy.contains('Task 1').should('be.visible');
-    cy.contains('Task 2').should('be.visible');
-    cy.get('li[data-testid="todo-item"]').should('have.length', 2);
   });
 });
