c50cdb951dca7d36c4c5a968e46448e1da78d9fc
Victor Adolfsson <victoreadolfsson@gmail.com>
2024-12-04 11:26:08 +0100

    add cypress folder

--- frontend/cypress.config.mjs
+++ frontend/cypress.config.mjs
@@ -1,26 +1,19 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   component: {
     component: {
       devServer: {
-        framework: "react",
-        bundler: "vite",
+        framework: 'react',
+        bundler: 'vite',
       },
     },
     supportFile: false,
   },
 
   e2e: {
-    baseUrl: "http://localhost:5173",
-    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    baseUrl: 'http://localhost:5173',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: false,
-  },
-
-  component: {
-    devServer: {
-      framework: "react",
-      bundler: "vite",
-    },
   },
 });
