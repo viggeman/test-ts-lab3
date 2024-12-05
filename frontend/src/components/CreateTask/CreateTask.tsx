3ac48a1e6790988c57f0e6d16aca11cbed8549ee
Victor Adolfsson <victoreadolfsson@gmail.com>
2024-12-05 14:12:35 +0100

    pass final test

--- frontend/src/components/CreateTask/CreateTask.tsx
+++ frontend/src/components/CreateTask/CreateTask.tsx
@@ -1,13 +1,26 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 interface CreateTaskProps {
   onCreateTask: (task: string) => void;
 }
 
 const CreateTask: FC<CreateTaskProps> = ({ onCreateTask }) => {
+  const [task, setTask] = useState('');
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    onCreateTask(task);
+  };
+
   return (
-    <form>
-      <input type="text" data-testid="task-input" />
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        data-testid="task-input"
+      />
       <button type="submit">Create</button>
     </form>
   );
